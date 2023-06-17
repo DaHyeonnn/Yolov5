8c301faff8f08e50126cfc871b7da695be915432
다현 <90945094+DaHyeonnn@users.noreply.github.com>
2023-06-17 17:12:30 +0900

    docs : camera_viewer_realsense 코드 설명 추가

--- scripts/camera_viewer_realsense.py
+++ scripts/camera_viewer_realsense.py
@@ -10,17 +10,20 @@ import pyrealsense2 as rs
 import numpy as np
 import cv2
 
+"""
+RealSense 카메라를 사용하여 실시간 이미지 처리 및 표시
+"""
 # Configure depth and color streams
-pipeline = rs.pipeline()
+pipeline = rs.pipeline() #realsense pipeline 생성
 config = rs.config()
 
 # Get device product line for setting a supporting resolution
-pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+pipeline_wrapper = rs.pipeline_wrapper(pipeline) 
 pipeline_profile = config.resolve(pipeline_wrapper)
-device = pipeline_profile.get_device()
+device = pipeline_profile.get_device() #디바이스 호출
 device_product_line = str(device.get_info(rs.camera_info.product_line))
 
-found_rgb = False
+found_rgb = False #RGB 카메라 여부
 for s in device.sensors:
     if s.get_info(rs.camera_info.name) == 'RGB Camera':
         found_rgb = True
@@ -39,7 +42,7 @@ if device_product_line != 'L500':
 pipeline.start(config)
 
 try:
-    while True:
+    while True: #무한루프를 통해 컬러 프레임을 계속해서 받아옴
 
         # Wait for a coherent pair of frames: depth and color
         frames = pipeline.wait_for_frames()
@@ -53,9 +56,9 @@ try:
         color_colormap_dim = color_image.shape
         
         images = np.stack(color_image)
-        images = cv2.flip(images, 1)
-        # Show images
+        images = cv2.flip(images, 1) #좌우반전
         
+        # Show image
         cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
         cv2.imshow('RealSense', images)
         cv2.waitKey(1)
